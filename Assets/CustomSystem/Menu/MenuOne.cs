1365eb4fbcea7b9990933da11afc427e2b9e9305
THIAGO ZACARIAS DA SILVA <thiago.silva311@fatec.sp.gov.br>
2020-04-26 12:16:48 -0300

    First implementation menu selection

---
+++ Assets/CustomSystem/Menu/MenuOne.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using CustomSystem;
+using UnityEngine.UI;
+
+namespace CustomSystem.Menu
+{
+    public class MenuOne : MonoBehaviour, INavigationSystem
+    {
+        [SerializeField] private int menuIndex;
+        public Button[] buttons;
+
+        private void Awake()
+        {
+            NavigationController.instance.currentNavigationSystem = this;
+        }
+
+        public void SelectCharacterMenu()
+        {
+            MainMenuManager.instance.RandomCharacters();
+            MainMenuManager.currentMenu = MainMenuManager.CurrentMenu.CharacterSelect;
+            MainMenuManager.instance.ChangeMenu();
+        }
+        
+        public void OnConfirm(int x, int y)
+        {
+            Debug.Log("Confirm menu one");
+            switch (y)
+            {
+                case 0:
+                    NavigationController.instance.ChangeMenu(menuIndex, Action.OnConfirm);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+            buttons[y].onClick.Invoke();
+        }
+
+        public void OnCancel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector2 OnLeft(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector2 OnRight(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector2 OnUp(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector2 OnDown(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/CustomSystem/NavigationController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+
+namespace CustomSystem
+{
+    public class NavigationController : LegacyInputImplementation
+    {
+        public static NavigationController instance;
+        public INavigationSystem currentNavigationSystem;
+        public GameObject[] menus;
+
+        public int currentNavigationX;
+        public int currentNavigationY;
+        public int maxNavigationX;
+        public int maxNavigationY;
+
+        private float _delayToChangeDirection = 0.5f;
+        private float _auxDelayToChangeDirection;
+        
+        
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            SetJoystick(JoystickIndex.JoystickOne);
+        }
+
+        private void Update()
+        {
+            ChangeSelectedOption(ButtonDirection().x, ButtonDirection().y);
+            if (ButtonA())
+            {
+                currentNavigationSystem.OnConfirm(currentNavigationX, currentNavigationY);
+            }
+        }
+
+        private void ChangeSelectedOption(float x, float y)
+        {
+            _auxDelayToChangeDirection += Time.deltaTime;
+            if(x == 0 || y == 0) return;;
+            if (_auxDelayToChangeDirection >= _delayToChangeDirection)
+            {
+                _auxDelayToChangeDirection = 0;
+            }
+        }
+
+
+        public void ChangeMenu(int menuIndex, Action action)
+        {
+            switch (menuIndex)
+            {
+                case 0:
+                    SetCurrentMenu(action == Action.OnConfirm ? 1 : 0);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+        }
+
+        private void SetCurrentMenu(int index)
+        {
+            foreach (var temp in menus)
+            {
+                temp.SetActive(false);
+            }
+            menus[index].SetActive(true);
+            currentNavigationX = 0;
+            currentNavigationY = 0;
+        }
+    }
+
+    public interface INavigationSystem
+    {
+        
+        void OnConfirm(int x, int y);
+        void OnCancel();
+        Vector2 OnLeft(int x, int y);
+        Vector2 OnRight(int x, int y);
+        Vector2 OnUp(int x, int y);
+        Vector2 OnDown(int x, int y);
+    }
+
+    public enum Action
+    {
+        OnConfirm,
+        OnCancel
+    }
+}
